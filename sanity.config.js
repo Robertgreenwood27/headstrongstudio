5612b7df02a286cdd2669891332508955f49ba2d
Robert Greenwood <RobertGreenwood27@gmail.com>
2023-03-03 11:23:46 -0700

    changed project name

--- sanity.config.js
+++ sanity.config.js
@@ -5,7 +5,7 @@ import {schemaTypes} from './schemas'
 
 export default defineConfig({
   name: 'default',
-  title: 'Sanity Project',
+  title: 'Headsrong Studio',
 
   projectId: 'etxmd4lr',
   dataset: 'production',
